2de7d141eaa4196a5c20b24979acd48a0ea7d945
Mohasin-Haque <mohasinhaque23121@gmail.com>
2022-02-25 19:08:03 +0530

    Feat - Created the layout of the app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 
+const Card = ({ title, description }) => {
+  return(
+<     div className="card">
+        <p>{title}</p>
+        <p>{description}</p>
+      </div>
+  )
+      
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>NotesApp</h2>
+
+      <div>
+        <form className='input-field'>
+        <input placeholder='Note title' />
+        <textarea placeholder='Enter the note here' />
+        </form>
+        <button>Add Note</button>
+        <section className='note-card'>
+        < Card title="Exam" description= "Exam aa gya h babu kuch pdhai krlo" />
+        < Card />
+        < Card />
+        < Card />
+        </section>
+        
+      
+
+      </div>
     </div>
   );
 }
